890f64df9eab39e500484f1a9c0defcc28db103a
Pablo Figueroa <apfigueroa3@utpl.edu.ec>
2022-06-01 08:27:56 -0500

    Commit Datos

--- AdministrarInmobiliaria/src/paquete01/Ejecutor.java
+++ AdministrarInmobiliaria/src/paquete01/Ejecutor.java
@@ -6,6 +6,9 @@
 package paquete01;
 
 import java.util.Scanner;
+import paquete02.Propietario;
+import paquete03.Barrio;
+import paquete04.Ciudad;
 
 /**
  *
@@ -60,22 +63,49 @@ public class Ejecutor {
     }
 
     public static void ingresarPropietario() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese los nombres del Propietario");
+        String nombreP = entrada.nextLine();
+        System.out.println("ingrese los apellidos del Porpietario");
+        String apellidosP = entrada.nextLine();
+        System.out.println("Ingrese la identificacion del Propietario");
+        String ID = entrada.nextLine();
+        Propietario p1 = new Propietario(nombreP, apellidosP, ID);
 
     }
 
     public static void ingresarBarrio() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del barrio");
+        String barrio = entrada.nextLine();
+        System.out.println("Ingrese una Referencia");
+        String referencia = entrada.nextLine();
+        Barrio b1 = new Barrio(barrio, referencia);
 
     }
 
     public static void ingresarCiudad() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese el nombre de la Ciudad");
+        String ciudad = entrada.nextLine();
+        System.out.println("Ingrese el nombre de la Provincia");
+        String provincia = entrada.nextLine();
+        Ciudad c1 = new Ciudad(ciudad,provincia);
 
     }
 
     public static void ingresarConstructora() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese el nombre de la Constructora");
+        String nombreC = entrada.nextLine();
+        System.out.println("Ingrese el ID de la Empresa");
+        String ID = entrada.nextLine();
+        Ciudad c1 = new Ciudad(nombreC,ID);
 
     }
 
     public static void ingresarCasa() {
+        
 
     }
 
